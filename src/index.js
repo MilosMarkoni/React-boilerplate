cb2c06338207fc9777008baddbc8d3bca33e397d
MilosMarkoni <markovicm04@gmail.com>
2019-10-24 13:14:57 +0200

    Add initial project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return <div className="App">Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
